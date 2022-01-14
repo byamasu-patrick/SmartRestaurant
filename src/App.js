d35a335d0508c4cae6c378e58fe56e181c8a2e64
Byamasu Patrick Paul <ptrckbyamasu@gmail.com>
2022-01-14 19:54:17 +0200

    Add menu components with dishes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      dishes: DISHES
     };
   }
 
   render(){
     return (
-      <div className = "App">
+      <div>
         <Navbar dark color = "primary">
           <div className = "container">
             <NavbarBrand href = "/">Restaurant Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu  dishes = { this.state.dishes }/>
       </div>
     );
   }
 }
+
+
 
 export default App;
